a65ac6b492eb2f169a7b4b2b6f911cc2069337a0
sia <sia.shadpey@mail.utoronto.ca>
2020-06-30 19:07:58 -0400

    rebuilding site Tue 30 Jun 2020 19:07:58 EDT

--- projects/covid19/html_region_plot.py
+++ projects/covid19/html_region_plot.py
@@ -8,8 +8,13 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 
-#fig =px.scatter(x=range(10), y=range(10))
-#fig.write_html("us.html")
+import yaml
+
+def get_population(region):
+    with open('population.json') as json_file:
+        for line in yaml.safe_load(json_file):
+            if line['region'].lower() == region:
+                return int(line['population'])
 
 def covid19_html_plot(dirpath, region):
 	dirpath = dirpath + os.path.sep + region
@@ -26,32 +31,51 @@ def covid19_html_plot(dirpath, region):
 
 	# y sims
 	y_sims = np.genfromtxt(dirpath + os.path.sep + "y_sims.csv", delimiter=',')
-	y_mean =  np.mean(y_sims, axis=0).astype(int)
-	y_lower = np.percentile(y_sims, q=2.5, axis=0).astype(int)
-	y_upper = np.percentile(y_sims, q=97.5, axis=0).astype(int)
+	y_mean =  np.mean(y_sims, axis=0)
+	y_lower = np.percentile(y_sims, q=2.5, axis=0)
+	y_upper = np.percentile(y_sims, q=97.5, axis=0)
 	
+	trace1 = go.Scatter(x=dates, y=y_upper.astype(int), name = '95% confidence interval', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', showlegend=False)
+	trace2 = go.Scatter(x=dates, y=y_lower.astype(int), fill='tonexty', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', name='95% confidence interval')
+	trace3 = go.Scatter(x=dates, y=y_mean.astype(int), name = 'mean', line_color='rgb(0,100,80)', line=dict(width = 4))
+	trace4 = go.Scatter(x=dates, y=y_obs.astype(int), name = 'reported data (7-day average)', mode = 'markers', line_color='rgb(36, 21, 113)', marker=dict(size=10))
+
+	n_pop = get_population(region)
+	scale = 1E6/n_pop
+
+	trace10 = go.Scatter(x=dates, y=(y_upper*scale), name = '95% confidence interval', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', showlegend=False, visible=False)
+	trace20 = go.Scatter(x=dates, y=(y_lower*scale), fill='tonexty', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', name='95% confidence interval', visible=False)
+	trace30 = go.Scatter(x=dates, y=(y_mean*scale ), name = 'mean', line_color='rgb(0,100,80)', line=dict(width = 4), visible=False)
+	trace40 = go.Scatter(x=dates, y=(y_obs*scale  ), name = 'reported data (7-day average)', mode = 'markers', line_color='rgb(36, 21, 113)', marker=dict(size=10), visible=False)
+
+	# TODO: interactive daily/cumm
 	#y_obs_cumm = np.cumsum(y_obs)
 	#y_mean_cumm = np.cumsum(y_mean)
 	#y_lower_cumm = np.cumsum(y_lower)
 	#y_upper_cumm = np.cumsum(y_upper)
-	
-	trace1 = go.Scatter(x=dates, y=y_upper, name = '95% confidence interval', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', showlegend=False)
-	trace2 = go.Scatter(x=dates, y=y_lower, fill='tonexty', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', name='95% confidence interval')
-	trace3 = go.Scatter(x=dates, y=y_mean, name = 'mean', line_color='rgb(0,100,80)', line=dict(width = 4))
-	trace4 = go.Scatter(x=dates, y=y_obs, name = 'reported data (7-day average)', mode = 'markers', line_color='rgb(36, 21, 113)', marker=dict(size=10))
+#
+	#trace100 = go.Scatter(x=dates, y=y_upper_cumm.astype(int), name = '95% confidence interval', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', showlegend=False, visible=False)
+	#trace200 = go.Scatter(x=dates, y=y_lower_cumm.astype(int), fill='tonexty', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', name='95% confidence interval', visible=False)
+	#trace300 = go.Scatter(x=dates, y=y_mean_cumm.astype(int), name = 'mean', line_color='rgb(0,100,80)', line=dict(width = 4), visible=False)
+	#trace400 = go.Scatter(x=dates, y=y_obs_cumm.astype(int), name = 'reported data (7-day average)', mode = 'markers', line_color='rgb(36, 21, 113)', marker=dict(size=10), visible=False)
+#
+	#trace1000 = go.Scatter(x=dates, y=y_upper_cumm*scale, name = '95% confidence interval', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', showlegend=False, visible=False)
+	#trace2000 = go.Scatter(x=dates, y=y_lower_cumm*scale, fill='tonexty', fillcolor='rgba(0,100,80,0.2)', line_color='rgba(255,255,255,0)', name='95% confidence interval', visible=False)
+	#trace3000 = go.Scatter(x=dates, y=y_mean_cumm*scale, name = 'mean', line_color='rgb(0,100,80)', line=dict(width = 4), visible=False)
+	#trace4000 = go.Scatter(x=dates, y=y_obs_cumm*scale, name = 'reported data (7-day average)', mode = 'markers', line_color='rgb(36, 21, 113)', marker=dict(size=10), visible=False)
 
 
-	data = [trace1, trace2, trace3, trace4]
+	data = [trace1, trace2, trace3, trace4, trace10, trace20, trace30, trace40] #, trace100, trace200, trace300, trace400, trace1000, trace2000, trace3000, trace4000]
 
-	updatemenus = list([
-	dict(active=1,
+	updatemenus = []
+	updatemenus.append(dict(active=0,
 	     buttons=list([
+	     	dict(label='Linear',
+	             method='relayout',
+	             args=[{'yaxis.type': 'linear'}]),
 	        dict(label='Log',
-	             method='update',
-	             args=[{'visible': [True, True, True, True, True]}, {'yaxis': {'type': 'log', 'title': 'Daily new cases'}}]),
-	        dict(label='Linear',
-	             method='update',
-	             args=[{'visible': [True, True, True, True, True]}, {'yaxis': {'type': 'linear', 'title': 'Daily new cases'}}])
+	             method='relayout',
+	             args=[{'yaxis.type': 'log'}])
 	        ]),
 	     type = "buttons",
 	     direction="right",
@@ -60,34 +84,36 @@ def covid19_html_plot(dirpath, region):
 		 xanchor="left",
 		 y=1.12,
 		 yanchor="top"
-	    )
-	])
+	    ))
+
+	updatemenus.append(
+	dict(active=0,
+	     buttons=list([
+	        dict(label='Total',
+	             method='restyle',
+	             args=[{'visible': [True, True, True, True, False, False, False, False, False, False, False, False, False, False, False, False]}]),
+	        dict(label='Per million habitants',
+	             method='restyle',
+	             args=[{'visible': [False, False, False, False, True, True, True, True, False, False, False, False, False, False, False, False]}])
+	        ]),
+	     type = "buttons",
+	     direction="right",
+		 showactive=True,
+		 x=0.2,
+		 xanchor="left",
+		 y=1.12,
+		 yanchor="top"
+	    ))
+
+	#print(updatemenus[0]['buttons'][updatemenus[0]['active']]['args'][0]['visible'])
+	#print(updatemenus[0]['buttons'][updatemenus[0]['active']]['args'][1]['yaxis']['title'])
+	#  args=[{'visible': updatemenus[0]['buttons'][updatemenus[0]['active']]['args'][0]['visible']}, {'yaxis': {'type': 'linear',  'title': updatemenus[0]['buttons'][updatemenus[0]['active']]['args'][1]['yaxis']['title'] }}]),
 
 	# plots
-	fig = go.Figure(data=data, layout=dict(updatemenus=updatemenus, yaxis_title='Daily new cases',
-	                  xaxis_title='Date',
-	                  template='simple_white'))
-
-
-
-	#fig.update_layout(
-    #annotations=[
-    #    dict(text="Log/Linear", x=0, xref="paper", y=1.1, yref="paper",
-    #                         align="left", showarrow=False),
-    #    dict(text="Total/Per million", x=0.25, xref="paper", y=1.1,
-    #                         yref="paper", showarrow=False),
-    #    dict(text="Daily/Cummulative", x=.5, xref="paper", y=1.1, yref="paper",
-    #                         showarrow=False)
-    #])
-
-
-	# TODO: interactive total/per million
-
-	# TODO: interactive daily/cumm
+	fig = go.Figure(data=data, layout=dict(updatemenus=updatemenus, yaxis_title = 'Daily new cases', xaxis_title='Date', template='simple_white'))
 
 	# save to html 
-
-	fig.write_html(region + ".html")
+	fig.write_html(region + ".html", config={'displaylogo':False})
 
 def main():
 	parser = argparse.ArgumentParser(description="Generates the html plot of the specified region.")
